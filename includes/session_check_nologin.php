db7a4ffcc0956e9aa26b5ed4c7e6650f7f2b5c9a
Yusuf Rivai <uriemailtest@gmail.com>
2025-11-13 05:13:06 +0700

    no login

--- includes/session_check_nologin.php
+++ includes/session_check_nologin.php
@@ -5,10 +5,10 @@
   // print_r($_SESSION);
   // print_r($_COOKIE);
 
-  if(!isset($_SESSION["session_username"])) {
-    // header("location: http://localhost:8888/html/uri/alhidayah/login.php");
-    header("location: https://alhidayah.infodkm.com/login.php");
-    exit();
-  }
+  // if(!isset($_SESSION["session_username"])) {
+  //   header("location: http://localhost:8888/html/uri/alhidayah/login.php");
+  //   // header("location: https://alhidayah.infodkm.com/login.php");
+  //   exit();
+  // }
 
 ?>
